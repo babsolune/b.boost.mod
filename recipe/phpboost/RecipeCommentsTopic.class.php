913a75ef360fa2df2932437f445e7519bbf84305
babsolune <babsolune@phpboost.com>
2023-03-29 06:03:05 +0200

    [NEW] recipe

---
+++ recipe/phpboost/RecipeCommentsTopic.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @copyright   &copy; 2005-2023 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version     PHPBoost 6.0 - last update: 2023 01 16
+ * @since       PHPBoost 6.0 - 2022 08 26
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
+ */
+
+class RecipeCommentsTopic extends DefaultCommentsTopic
+{
+	public function __construct(RecipeItem $item = null)
+	{
+		parent::__construct('recipe');
+		$this->item = $item;
+	}
+
+	protected function get_item_from_manager()
+	{
+		return RecipeService::get_item($this->get_id_in_module());
+	}
+}
+?>
