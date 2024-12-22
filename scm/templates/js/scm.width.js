c64a8c803199cebf0d923f1ef6736c1c0eb29fad
babsolune <babsolune@phpboost.com>
2024-12-22 08:29:50 +0100

    [UPD] scm | display

--- scm/templates/js/scm.width.js
+++ scm/templates/js/scm.width.js
@@ -18,30 +18,24 @@ document.addEventListener('DOMContentLoaded', () => {
         const classList = width.classList;
         const classes = width.className.split(' ');
         // Static
-        if (window.innerWidth < 769) {
             classes.forEach(sizeClass => {
-                if (sizeClass.startsWith('md-width-')) classList.remove(sizeClass);
-                if (sizeClass.startsWith('lg-width-')) classList.remove(sizeClass);
                 if (sizeClass.startsWith('sm-width-')) {
                     const size = sizeClass.split('-');
                     if (size[2] == "pc") size[2] = '%';
                     width.style.width = size[3] + size[2];
                 }
             });
-        }
+        // }
         // Dynamic
         const resizeHandler = () => {
-            if (window.innerWidth < 769) {
                 classes.forEach(sizeClass => {
-                    if (sizeClass.startsWith('md-width-')) classList.remove(sizeClass);
-                    if (sizeClass.startsWith('lg-width-')) classList.remove(sizeClass);
                     if (sizeClass.startsWith('sm-width-')) {
                         const size = sizeClass.split('-');
                         if (size[2] == "pc") size[2] = '%';
                         width.style.width = size[3] + size[2];
                     }
                 });
-            }
+            // }
         }
         window.addEventListener('resize', resizeHandler);
     });
@@ -51,8 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
         // Static
         if (window.innerWidth >= 769) {
             classes.forEach(sizeClass => {
-                    if (sizeClass.startsWith('sm-width-')) classList.remove(sizeClass);
-                    if (sizeClass.startsWith('lg-width-')) classList.remove(sizeClass);
                 if (sizeClass.startsWith('md-width-')) {
                     const size = sizeClass.split('-');
                     if (size[2] == "pc") size[2] = '%';
@@ -64,8 +56,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const resizeHandler = () => {
             if (window.innerWidth >= 769) {
                 classes.forEach(sizeClass => {
-                    if (sizeClass.startsWith('sm-width-')) classList.remove(sizeClass);
-                    if (sizeClass.startsWith('lg-width-')) classList.remove(sizeClass);
                     if (sizeClass.startsWith('md-width-')) {
                         const size = sizeClass.split('-');
                         if (size[2] == "pc") size[2] = '%';
@@ -82,8 +72,6 @@ document.addEventListener('DOMContentLoaded', () => {
         // Static
         if (window.innerWidth >= 1366) {
             classes.forEach(sizeClass => {
-                if (sizeClass.startsWith('sm-width-')) classList.remove(sizeClass);
-                if (sizeClass.startsWith('md-width-')) classList.remove(sizeClass);
                 if (sizeClass.startsWith('lg-width-')) {
                     const size = sizeClass.split('-');
                     if (size[2] == "pc") size[2] = '%';
@@ -95,8 +83,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const resizeHandler = () => {
             if (window.innerWidth >= 1366) {
                 classes.forEach(sizeClass => {
-                    if (sizeClass.startsWith('sm-width-')) classList.remove(sizeClass);
-                    if (sizeClass.startsWith('md-width-')) classList.remove(sizeClass);
                     if (sizeClass.startsWith('lg-width-')) {
                         const size = sizeClass.split('-');
                         if (size[2] == "pc") size[2] = '%';
@@ -107,103 +93,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         window.addEventListener('resize', resizeHandler);
     });
-
-    // Manage columns
-    // const sm_cols = document.querySelectorAll('[class*="sm-col-"]');
-    // const md_cols = document.querySelectorAll('[class*="md-col-"]');
-    // const lg_cols = document.querySelectorAll('[class*="lg-col-"]');
-
-    // sm_cols.forEach((col) => {
-    //     const classList = col.classList;
-    //     const classes = col.className.split(' ');
-    //     // Static
-    //     if (window.innerWidth < 769) {
-    //         classes.forEach(colClass => {
-    //             children = colClass.querySelectorAll(':scope > *');
-    //             if (colClass.startsWith('md-col-')) classList.remove(colClass);
-    //             if (colClass.startsWith('lg-col-')) classList.remove(colClass);
-    //             if (colClass.startsWith('sm-col-')) {
-    //                 const size = colClass.split('-');
-    //                 children.style.width = 'calc(100% / ' + size[2] + ' - var(--cell-gap) - 1px)';
-    //             }
-    //         });
-    //     }
-    //     // Dynamic
-    //     const resizeHandler = () => {
-    //         if (window.innerWidth < 769) {
-    //             classes.forEach(colClass => {
-    //                 children = colClass.querySelectorAll(':scope > *');
-    //                 if (colClass.startsWith('md-col-')) classList.remove(colClass);
-    //                 if (colClass.startsWith('lg-col-')) classList.remove(colClass);
-    //                 if (colClass.startsWith('sm-col-')) {
-    //                     const size = colClass.split('-');
-    //                     children.style.width = 'calc(100% / ' + size[2] + ' - var(--cell-gap) - 1px)';
-    //                 }
-    //             });
-    //         }
-    //     }
-    //     window.addEventListener('resize', resizeHandler);
-    // });
-    // md_cols.forEach((col) => {
-    //     const classList = col.classList;
-    //     const classes = col.className.split(' ');
-    //     // Static
-    //     if (window.innerWidth >= 769) {
-    //         classes.forEach(colClass => {
-    //             children = colClass.querySelectorAll(':scope > *');
-    //             if (colClass.startsWith('sm-col-')) classList.remove(colClass);
-    //             if (colClass.startsWith('lg-col-')) classList.remove(colClass);
-    //             if (colClass.startsWith('md-col-')) {
-    //                 const size = colClass.split('-');
-    //                 children.style.width = 'calc(100% / ' + size[2] + ' - var(--cell-gap) - 1px)';
-    //             }
-    //         });
-    //     }
-    //     // Dynamic
-    //     const resizeHandler = () => {
-    //         if (window.innerWidth >= 769) {
-    //             classes.forEach(colClass => {
-    //                 children = colClass.querySelectorAll(':scope > *');
-    //                 if (colClass.startsWith('sm-col-')) classList.remove(colClass);
-    //                 if (colClass.startsWith('lg-col-')) classList.remove(colClass);
-    //                 if (colClass.startsWith('md-col-')) {
-    //                     const size = colClass.split('-');
-    //                     children.style.width = 'calc(100% / ' + size[2] + ' - var(--cell-gap) - 1px)';
-    //                 }
-    //             });
-    //         }
-    //     }
-    //     window.addEventListener('resize', resizeHandler);
-    // });
-    // lg_cols.forEach((col) => {
-    //     const classList = col.classList;
-    //     const classes = col.className.split(' ');
-    //     // Static
-    //     if (window.innerWidth >= 1366) {
-    //         classes.forEach(colClass => {
-    //             children = colClass.querySelectorAll(':scope > *');
-    //             if (colClass.startsWith('sm-col-')) classList.remove(colClass);
-    //             if (colClass.startsWith('md-col-')) classList.remove(colClass);
-    //             if (colClass.startsWith('lg-col-')) {
-    //                 const size = colClass.split('-');
-    //                 children.style.width = 'calc(100% / ' + size[2] + ' - var(--cell-gap) - 1px)';
-    //             }
-    //         });
-    //     }
-    //     // Dynamic
-    //     const resizeHandler = () => {
-    //         if (window.innerWidth >= 1366) {
-    //             classes.forEach(colClass => {
-    //                 children = colClass.querySelectorAll(':scope > *');
-    //                 if (colClass.startsWith('sm-col-')) classList.remove(colClass);
-    //                 if (colClass.startsWith('md-col-')) classList.remove(colClass);
-    //                 if (colClass.startsWith('lg-col-')) {
-    //                     const size = colClass.split('-');
-    //                     col.style.width = 'calc(100% / ' + size[2] + ' - var(--cell-gap) - 1px)';
-    //                 }
-    //             });
-    //         }
-    //     }
-    //     window.addEventListener('resize', resizeHandler);
-    // });
 });
